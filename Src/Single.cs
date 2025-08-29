8cc235a4ad164d460686c1282e3b9b5e10baa9c4
danila.golovenko <dgolovenko01@gmail.com>
2025-08-29 09:59:59 +0400

    upd: single

---
+++ Src/Single.cs
@@ -0,0 +1,68 @@
+﻿using SelfishFramework.Src.Core;
+using SelfishFramework.Src.Core.Components;
+using SelfishFramework.Src.Core.Filter;
+using SelfishFramework.Src.SLogs;
+
+namespace SelfishFramework.Src
+{
+    public readonly struct Single<T> where T : struct, IComponent
+    {
+        private readonly Filter _filter;
+
+        public Single(World world)
+        {
+            _filter = world.Filter.With<T>().Build();
+        }
+        
+        public bool Exists()
+        {
+            return _filter.Count > 0;
+        }
+        
+        public ref T Get(out bool exists)
+        {
+            if (_filter.Count > 1)
+            {
+                SLog.LogError("There are multiple entities with the same single component type: " + typeof(T).Name);
+            }
+            
+            foreach (var entity in _filter)
+            {
+                exists = true;
+                return ref entity.Get<T>();
+            }
+
+            exists = false;
+            return ref ComponentPool<T>.Default;
+        }
+        public ref T Get()
+        {
+            if (_filter.Count > 1)
+            {
+                SLog.LogError("There are multiple entities with the same single component type: " + typeof(T).Name);
+            }
+            
+            foreach (var entity in _filter)
+            {
+                return ref entity.Get<T>();
+            }
+
+            return ref ComponentPool<T>.Default;
+        }
+
+        public Entity GetEnt()
+        {
+            if (_filter.Count > 1)
+            {
+                SLog.LogError("There are multiple entities with the same single component type: " + typeof(T).Name);
+            }
+            
+            foreach (var entity in _filter)
+            {
+                return entity; 
+            }
+
+            return default;
+        }
+    }
+}
