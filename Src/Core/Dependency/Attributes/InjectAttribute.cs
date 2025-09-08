a3f23821649ab06de309022a248af7b7222cc779
danila.golovenko <dgolovenko01@gmail.com>
2025-09-08 23:48:51 +0400

    upd: namescapes

--- Src/Core/Dependency/Attributes/InjectAttribute.cs
+++ Src/Core/Dependency/Attributes/InjectAttribute.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace SelfishFramework.Src.Core.Attributes
+namespace SelfishFramework.Src.Core.Dependency.Attributes
 {
     [AttributeUsage(AttributeTargets.Field)]
     public class InjectAttribute : Attribute
--- Src/Core/Dependency/Attributes/InjectableAttribute.cs
+++ Src/Core/Dependency/Attributes/InjectableAttribute.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace SelfishFramework.Src.Core.Attributes
+namespace SelfishFramework.Src.Core.Dependency.Attributes
 {
     [AttributeUsage(AttributeTargets.Class)]
     public class InjectableAttribute : Attribute
