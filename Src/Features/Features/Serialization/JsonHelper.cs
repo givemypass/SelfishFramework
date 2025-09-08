014344829fbe803dc3e2e9defc46c82daa2823be
danila.golovenko <dgolovenko01@gmail.com>
2025-09-09 00:01:56 +0400

    add: jsonHelper

---
+++ Src/Features/Features/Serialization/JsonHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace SelfishFramework.Src.Features.Features.Serialization
+{
+    public static class JsonHelper
+    {
+        public static bool TryLoadJson(string path, out string json)
+        {
+            if (File.Exists(path))
+            {
+                json = File.ReadAllText(path);
+                return true;
+            }
+            json = default;
+            return false;
+        }
+
+        public static void SaveJson(string path, string json)
+        {
+            File.WriteAllText(path, json);
+        }
+    }
+}
