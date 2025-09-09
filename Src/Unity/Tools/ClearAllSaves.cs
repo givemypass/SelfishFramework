7a57a6032de9c6f5eaab8ee5c36cdcfcc3d7e054
danila.golovenko <dgolovenko01@gmail.com>
2025-09-09 22:19:59 +0400

    add: clear all save tool

---
+++ Src/Unity/Tools/ClearAllSaves.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+
+namespace SelfishFramework.Src.Unity.Tools
+{
+    public static class ClearAllSaves
+    {
+        [MenuItem("Selfish/Tools/ClearAllSaves")]
+        public static void Clear()
+        {
+            var di = new System.IO.DirectoryInfo(UnityEngine.Application.persistentDataPath);
+            foreach (var file in di.GetFiles())
+            {
+                file.Delete(); 
+            }
+            foreach (var dir in di.GetDirectories())
+            {
+                dir.Delete(true); 
+            }
+        } 
+    }
+}
