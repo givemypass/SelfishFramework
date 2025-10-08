46e7dcccecb1d938c0fe874947ba3d895520f8af
danila.golovenko <dgolovenko01@gmail.com>
2025-10-08 23:13:59 +0400

    add: save when exit mono component

---
+++ Src/Unity/CommonMono/SaveWhenExitMonoComponent.cs
@@ -0,0 +1,37 @@
+﻿using SelfishFramework.Src.Core;
+using SelfishFramework.Src.Features.GameFSM.Components;
+using SelfishFramework.Src.Unity.CommonCommands;
+using UnityEngine;
+
+namespace SelfishFramework.Src.Unity.CommonMono
+{
+    public class SaveWhenExitMonoComponent : MonoBehaviour
+    {
+#if !UNITY_EDITOR
+        private void OnApplicationPause(bool onPause)
+        {
+            if (!onPause)
+                return;
+            if (!SManager.IsAlive)
+            {
+                return;
+            }
+            var filter = SManager.World.Filter.With<GameStateComponent>().Build();
+            foreach (var entity in filter)
+            {
+                ref var gameStateComponent = ref entity.Get<GameStateComponent>();
+                if (gameStateComponent.CurrentState == 0) continue;
+                SManager.World.Command(new BeforeSaveCommand());
+                SManager.World.Command(new SaveCommand());
+            }
+        }
+#endif
+#if UNITY_EDITOR
+        private void OnApplicationQuit()
+        {
+            SManager.World.Command(new BeforeSaveCommand());
+            SManager.World.Command(new SaveCommand());
+        }
+#endif
+    }
+}
